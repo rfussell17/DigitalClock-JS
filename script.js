496abd204402d6386213af21a8d99a58061ceed2
Robin Fussell <rfussell17@gmail.com>
2019-11-04 00:37:18 -0800

    still trying to fix 0:37:05 time display

--- script.js
+++ script.js
@@ -3,35 +3,34 @@
 //create function to display time
 
 function displayTime() {
-const time = new Date();
-const hours = time.getHours() % 12;
-const minutes = time.getMinutes();
-const seconds = time.getSeconds();
-const ampm = time.getHours() >=12 ? "PM" : "AM";
+  const time = new Date();
+  let hours = time.getHours() % 12;
+ // hours == 0? hours = 12 : time.getHours() % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  let ampm = time.getHours() < 12? "AM" : "PM";
+  hours == 0? hours = 12 : time.getHours() % 12;
+  //hours === 0? hours = 12 : hours() % 12;
+  //refactoring hours, minutes, seconds to display "0" before single digit numbers
+  
+  const refactoredHours = hours < 10? hours.innerText = "0" + time.getHours() : time.getHours();
+  const refactoredMinutes = minutes < 10? hours.innerText = "0" + time.getMinutes() : time.getMinutes();
+  const refactoredSeconds = seconds < 10? hours.innerText = "0" + time.getSeconds() : time.getSeconds();
+  
+  //assign newly refactored hours, min, sec to localTime & assign localTime to "time" class in innerHTML 
+  
+  const localTime = refactoredHours + ":" + refactoredMinutes + ":" + refactoredSeconds; 
+  document.getElementsByClassName("time")[0].innerHTML = localTime;
+  document.getElementsByClassName("ampm")[0].innerHTML = ampm;
+  }
+  
 
-//refactoring hours, minutes, seconds to display "0" before single digit numbers
+  function displayDate() {
+    const todaysDate = new Date();
+    document.getElementsByClassName("date")[0].innerHTML = todaysDate;
+  }
 
-const refactoredHours = hours < 10? hours.innerText = "0" + time.getHours() % 12 : time.getHours() % 12;
-const refactoredMinutes = minutes < 10? hours.innerText = "0" + time.getMinutes() : time.getMinutes();
-const refactoredSeconds = seconds < 10? hours.innerText = "0" + time.getSeconds() : time.getSeconds();
-
-//assign newly refactored hours, min, sec to localTime & assign localTime to "time" class in innerHTML 
-
-const localTime = refactoredHours + ":" + refactoredMinutes + ":" + refactoredSeconds; 
-document.getElementsByClassName("time")[0].innerHTML = localTime;
-document.getElementsByClassName("ampm")[0].innerHTML = ampm;
-}
-
-//create function to display date
-
-function displayDate(){
-  const todaysDate = new Date();
-  document.getElementsByClassName("date")[0].innerHTML = todaysDate;
-}
-
-//call time and date functions, refresh every second
-
-displayTime();
-setInterval(displayTime, 1000);
-displayDate();
-setInterval(displayDate, 1000);
+  //call time and date functions, refresh every second
+  displayDate();
+  displayTime();
+  setInterval(displayTime, 1000);
